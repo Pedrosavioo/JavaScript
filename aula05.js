f28893ae58b5364cb814dc30bff1a20bc2cbd68a
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-24 15:27:00 -0300

    Operadores relacionais

---
+++ aula05.js
@@ -0,0 +1,16 @@
+/*
+- - OPERADORES RELACIONAIS
+    >
+    >=
+    <
+    <= 
+    == igual
+      '-> IMPORTANTE: = concatenação
+    != diferente
+*/
+
+let n1 = 3;
+let n2 = 6;
+let n3 = 3;
+
+console.log(n1 == n3);
