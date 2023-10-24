3bec19a044e46b913544815d092a183351ce0312
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-24 15:26:39 -0300

    Operadores matemáticos

---
+++ aula04.js
@@ -0,0 +1,22 @@
+/*
+- - OPERADORES ARITMÉTICOS
+    +
+    -
+    *
+    /
+    %
+ 
+- OPERADORES DE INCREMENTO E DECREMENTO
+    ++
+    --
+    +=
+    -=
+*/
+
+let n1 = 3, n2 = 5;
+let res = n1 + n2;
+
+n1++;
+
+console.log(res);
+console.log(n1);
