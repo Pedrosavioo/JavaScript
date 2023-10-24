6215dae8d3910805cba25832a370f47eb80f61e3
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-24 15:27:45 -0300

    Operadores lógicos

---
+++ aula07.js
@@ -0,0 +1,14 @@
+/*
+  - OPERADORES LÓGICOS
+    && -> and (e)
+    || -> or (ou)
+    ! -> not (não)
+*/
+let n1 = 2;
+let n2 = 7
+
+if(!(n1 + 6 == n2) || n2 - 4 == n1){
+  console.log("entrou no IF");
+} else {
+  console.log("entrou no ELSE");
+}
