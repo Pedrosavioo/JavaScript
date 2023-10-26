2981734dac3d622e23bbaa79a2b755afbc797d15
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-26 13:41:19 -0300

    Loop FOR

---
+++ aula15.js
@@ -0,0 +1,17 @@
+/*
+  - FOR
+    '--> loop.
+    '--> repetir trecho de código.
+    '--> cada repetição que um loop executar é chamado de ITERAÇÃO.
+    '--> loops são classificados em DEFINIDOS e INDEFINIDOS.
+    '      '->DEFINIDOS: quantidade de vezes que SEI que vai repetir. (FOR)
+    '      '->INDEFINIDOS: quando NÃO SEI a quantidade de vezes que vai repetir. (WHILE e DO WHILE)
+    '-> for(variável contadora; condição; controle).
+           '-> EX: for(let i = 1; i <= 10; i++) // a variável contador começa em 1, vai ser somado +1 a variável contadora a cada iteração e quando a variável contadora for maior que 10 termina o for.
+
+*/
+
+//=== FOR ========================================================================================================================================
+  for(let i = 0; i < 10; i++){
+    console.log(i);
+  }
