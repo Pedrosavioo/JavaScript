fb1ec8113dea0b1160ae0b5df5b8d2ebb8a3d5e3
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-26 13:37:48 -0300

    Pré incremento(++x) e Pós incremento(x++)

---
+++ aula09.js
@@ -0,0 +1,18 @@
+/*
+  - DIFERENÇA ENTRE PRÉ INCREMENTO(++n) E PÓS INCREMENTO(n++)
+*/
+
+//-------------------------------------------------------------------------------------------------------------------------------------------
+// --> EXEMPLO: Pós incremento
+let n = 10
+
+console.log(n++); // primeiro vai ser mostrado o valor da variável e em seguida será feito o incremento. Resultando em uma saída 10 no console.
+console.log(n); // mostrando novamente a variável n no console temos que um novo valor foi atribuído a ela. Sendo assim a saída será 11.
+
+
+console.log('---------------------------');
+//-------------------------------------------------------------------------------------------------------------------------------------------
+// -> EXEMPLO: Pré incremento
+let num = 10
+
+console.log(++num); // note que aqui primeiro será incrementado e em seguida será mostrado. Resultando em uma saída 11 no console.
