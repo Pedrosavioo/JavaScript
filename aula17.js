1d531ff3494aa81869b6038b51656573954db297
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-26 13:42:02 -0300

    Loop WHILE

---
+++ aula17.js
@@ -0,0 +1,16 @@
+/*
+  - WHILE
+    '-->  "Enquanto".
+    '-->  loop indefinido, quando não tenho certeza a quantidade de vezes que vou iterar esse loop.
+*/
+
+//=== WHILE ========================================================================================================================================
+  let n = 0
+
+  while (n<10) { // Enquanto n for menor que 10
+    console.log(n); // Saída será o valor de n
+    n++ // atribuindo +1 no valor de n
+  }
+
+  console.log('fim'); // fim do programa
+//===========================================================================================================================================
