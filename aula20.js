ccde963c9f82bf32a92b328d65376d99887d5e66
Pedrosavioo <pedrosaviolima618@gmail.com>
2023-10-31 17:23:51 -0300

    Funções

---
+++ aula20.js
@@ -0,0 +1,18 @@
+/*
+  - FUNÇÕES
+      '--> Blocos de comando para executar em momento oportuno.
+*/
+
+//=== FUNÇÃO TRADICIONAL ========================================================================================================================================
+  // DECLARAÇÃO de Função SEM PARÂMETRO:
+  function nome(){
+    console.log('Hello world!');
+  }
+  nome() // Chamando a função nome.
+
+  // DECLARAÇÃO de Função COM PARÂMETRO:
+  function soma(a,b){
+    console.log(a + b);
+  }
+  soma(10,12); // Chamando a função soma, passando 10 e 12 como parâmetros para 'a' e 'b' respectivamente.
+  
